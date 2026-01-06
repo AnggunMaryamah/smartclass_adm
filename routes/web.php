0f16876ebaa6d0dc50de8402016188cb00e14e4c
Anggun <anggunmaryamah71013@gmail.com>
2026-01-06 11:44:53 +0700

    bagian-logout

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use App\Http\Controllers\Siswa\KuisController;
 use App\Http\Controllers\TinyMceUploadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\GuruRegisterController;
 use App\Http\Controllers\AdminGuruController;
